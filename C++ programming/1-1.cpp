4e1f11f7dd48b56e4c3a01efb61967e0e7f43610
seungminlll3420 <seungminlll3420@gmail.com>
2019-09-03 15:36:29 +0900

    1

---
+++ C++ programming/1-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "너비 입력: ";
+
+	int w;
+	cin >> w;
+
+	cout << "높이: ";
+	int h;
+	cin >> h;
+
+	int area = w + h;
+	cout << "면적은 " << area << "\n";
+
+}
