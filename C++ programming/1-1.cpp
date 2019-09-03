2976337d497ae274e2486f6701d54b4e09ecf3f9
seungminlll3420 <seungminlll3420@gmail.com>
2019-09-03 15:38:39 +0900

    한글 수정

--- C++ programming/1-1.cpp
+++ C++ programming/1-1.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-	cout << "너비 입력: ";
+	cout << "Width: ";
 
 	int w;
 	cin >> w;
 
-	cout << "높이: ";
+	cout << "height: ";
 	int h;
 	cin >> h;
 
 	int area = w + h;
-	cout << "면적은 " << area << "\n";
+	cout << "sum " << area << "\n";
 
 }
