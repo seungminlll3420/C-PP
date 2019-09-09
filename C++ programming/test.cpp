364687e92ef7c22e4471528b0b1bfb6e64d772b8
seungminlll3420 <45160518+seungminlll3420@users.noreply.github.com>
2019-09-09 19:04:07 +0900

    test

--- C++ programming/test.cpp
+++ C++ programming/test.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main(){
+  char text[10000];
+  fill_n(text,10000,'\0');
+  cout << "Please enter text. Draw a histogram.\n" << "The end of the text is ';'. Up to 1,000.\n";
+  cin.getline(text,10000,';');
+  int i=0,j;
+  int numberofalphabets[27];
+  fill_n(numberofalphabets,27,0);
+  char alphabets[27] ={'\0','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+  'o','p','q','r','s','t','u','v','w','x','y','z'};
 
-  string a;
-  a = "abcdefg";
-  cout << LeftStr(a,3);
+  while(text[i] != '\0'){
+    for(int g = 1; g < 27; g++){
+      if(tolower(text[i]) == alphabets[g]){
+        numberofalphabets[g]++;
+        numberofalphabets[0]++;
+      }
+    }
+    i++;
+  }
 
+  cout << "What's the total number of alphabets? "<< numberofalphabets[0];
+  cout << "\n";
+
+  for(int j = 1; j < 27 ; j++){
+    cout << alphabets[j] <<" ("<< numberofalphabets[j] << ") "<< ": ";
+    for(int l = 0 ; l < numberofalphabets[j]; l++){
+      cout << "*";
+    }
+    cout << "\n";
+  }
 }
