92bfaee94a48df2b703f8eafeec3fd4e8d56adbc
seungminlll3420 <45160518+seungminlll3420@users.noreply.github.com>
2019-11-26 20:52:02 +0900

    update

--- LINUX_PROJECT/source/main.cpp
+++ LINUX_PROJECT/source/main.cpp
@@ -1,9 +1,14 @@
 #include "cast.h"
 #include "vector_cast.h"
-
+#include "my_star.h"
+#include "community.h"
 int main() {
         vector_cast a;
         vector<cast> b;
+        vector<string> inmystar;
+        my_star Mystar;
+        community com;
+        vector<string> c;
         int age;
         string name;
         string En;
@@ -11,14 +16,14 @@ int main() {
         int out = 0;
         int key;
         while (out != 1) {
-                cout << "1 : 사람 추가하기 2 : 사람 삭제하기 3: 사람 수정하기 4 : 추가된 사람들 보기" << endl;
+                cout << "1 : 사람 추가하기 2 : 사람 삭제하기 3: 사람 수정하기 4 : 추가된 사람들 보기 5 : 3명 뽑기 ㅋ 6 : 글쓰기 " << endl;
                 cin >> key;
 
                 switch (key) {
                 case 1:
                         cout << "이름 나이 소속사를 적어" << endl;
                         cin >> name >> age >> En;
-                        a.cast_add(b,name, age, En);
+                        a.cast_add(b, name, age, En);
                         break;
                 case 2:
                         cout << "삭제할 사람의 이름을 적어봐" << endl;
@@ -33,6 +38,14 @@ int main() {
                 case 4:
                         a.show(b);
                         break;
+
+                case 5:
+                        Mystar.My_star(b, inmystar);
+
+                        break;
+                case 6:
+                        com.Community(c);
                 }
+
         }
 }
