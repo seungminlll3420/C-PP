8b8e704beb9d27e8366c04446b421e2663c08195
seungminlll3420 <45160518+seungminlll3420@users.noreply.github.com>
2019-11-26 14:08:28 +0900

    update

--- LINUX_PROJECT/추가기능/community/community.cpp
+++ LINUX_PROJECT/추가기능/community/community.cpp
@@ -1,11 +1,38 @@
 #include "community.h"
 
-string *community::textArray = new string[ARRAY_MAX];
-int community::num = 0;
 
-void community::add(string text) {
 
-}
-void community::print() {
 
+void community:: Community(vector<string> &com) {
+        int out =0;
+        int key = 0;
+        string ma;
+        while(out != 1) {
+                cout << "1 : 글쓰기 2: 글 보기 3 : 처음화면 가기"<<endl;
+                cin >> key;
+                switch(key) {
+                case 1:
+                        cout << "쓰세요" <<endl;
+                        cin >> ma;
+                        if(ma == "") {
+                                cout << "You've entered something wrong !!" << endl;
+                                break;
+                        }else{
+                                com.push_back(ma);
+                                if(com.size() == 11) com.erase(10);
+
+                        }
+                        break;
+                case 2:
+                        if(com.size() == 0) {
+                                cout<< "등록된 글이 없어,,,," << endl;
+                        }else{
+                                for(int i =0; i< com.size(); i++) cout << i << " :: " << com[i] <<endl;
+                        }
+                        break;
+                case 3:
+                        out =1;
+
+                }
+        }
 }
--- LINUX_PROJECT/추가기능/community/community.h
+++ LINUX_PROJECT/추가기능/community/community.h
@@ -5,9 +5,7 @@
 using namespace std;
 
 class community {
-        static string *textArray;
-        static int num; // 게시판에 올라간 글 수
 public:
-        static void add(string text); // 게시판에 글 추가하기
-        static void print(); // 게시판에 쓰여진 글 출력하기
+void community:: Community(vector<string> &com)
+
 };
