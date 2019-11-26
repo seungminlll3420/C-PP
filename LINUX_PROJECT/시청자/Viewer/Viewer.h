343e877d17bfe1bd7b3fddf72f1247c4b61f9524
seungminlll3420 <45160518+seungminlll3420@users.noreply.github.com>
2019-11-27 03:00:36 +0900

    update

--- LINUX_PROJECT/시청자/Viewer/Viewer.h
+++ LINUX_PROJECT/시청자/Viewer/Viewer.h
@@ -16,8 +16,6 @@ string getNumber();
 string getName();
 int getVotePresence();
 int getDonation();
-void setNumber(string PhoneNumber);   // 새로운 시청자에 대한 set함수
-void setPresence(int num);   // set함수 제거
 void vote(int num, string name);   // 최대 두명까지 가능한 투표 함수
 void donate(int money);   // 출연자에게 후원 함수
 vector<string> & getmystar();
